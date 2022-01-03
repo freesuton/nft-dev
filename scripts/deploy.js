7c57d7009e71dbdeb5412fa596d5bf5b81194878
slowpon <slowpon@outlook.com>
2022-01-03 06:08:52 -0500

    NFT OZ

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,13 +4,13 @@ const {ethers} = require("hardhat");
 async function main(){
 
   //1st to do is to get a compiled contract
-  const SuperMarioWorld = await ethers.getContractFactory("SuperMarioWorld");
-  const superMarioWorld = await SuperMarioWorld.deploy("Alaska SeaLife Center", "ASC");
+  const AnimalWorld = await ethers.getContractFactory("AnimalWorld");
+  const animalWorld = await AnimalWorld.deploy("Alaska SeaLife Center", "ASC");
 
-  await superMarioWorld.deployed();
-  console.log("Success! Contract was deployed to: ", superMarioWorld.address);
+  await animalWorld.deployed();
+  console.log("Success! Contract was deployed to: ", animalWorld.address);
 
-  await superMarioWorld.mint("https://ipfs.io/ipfs/QmZRpUvBFmrHruBAKasN2zTuVrYgNiXQ7kTtZ36eHvnLVm");
+  await animalWorld.mint("https://ipfs.io/ipfs/QmSpv8P84fwi6XkdFemhzKXJiJxZJsgnWiraWZtvarStLZ");
 
   console.log("NFT successfully minted");
 }
