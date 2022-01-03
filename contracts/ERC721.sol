da773dcf1db9d6e7ee7a60dac80f42e98f72f2d8
slowpon <slowpon@outlook.com>
2022-01-03 03:36:59 -0500

    NFT deployment

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -5,6 +5,9 @@ contract ERC721 {
 
     //we will want to search it all of these so we add index key word
     event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
+
+    event Approval(address indexed _owner, address indexed _approved, uint256 _tokenId);
+
     //searched by owners or operators
     //in order to let people to search through the event logs, we can add variable indexed
     event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved);
@@ -15,36 +18,39 @@ contract ERC721 {
 
     mapping(address => mapping(address => bool)) private _operatorApprovals;
 
+    mapping(uint256 => address) private _tokenApprovals;
+
     //when you put an token id, it get out the approved address
 
     //return the number of NFTs assigned to an owner
-    function balanceOf(address owner) public view returns(uint256){
-        //error checking is required: it is not a zero address
+    function balanceOf(address owner) public view returns(uint256) {
         require(owner != address(0), "Address is zero");
         return _balances[owner];
     }
 
-    //Return the owner of an NFT
-    function owner(uint256 tokenId) public view returns(address){
+    // Return the owner of an NFT
+    function ownerOf(uint256 tokenId) public view returns(address) {
         address owner = _owners[tokenId];
+        //error checking is required: it is not a zero address
         require(owner != address(0), "TokenID does not exist");
         return owner;
     }
 
     //Enable or disables an operator to manage all of msg.senders assets.
-    function setApprovalForAll(address operator, bool approved) public{
+    function setApprovalForAll(address operator, bool approved) public {
         _operatorApprovals[msg.sender][operator] = approved;
         emit ApprovalForAll(msg.sender, operator, approved);
     }
 
     //Checks if an address is an operator for another address
-    function isApprovedForAll(address owner, address operator) public view returns(bool){
+    function isApprovedForAll(address owner, address operator) public view returns(bool) {
         return _operatorApprovals[owner][operator];
     }
+
     //Updates an approved address for an NFT
     function approve(address to, uint256 tokenId) public {
         address owner = ownerOf(tokenId);
-        require(msg.sender == owner || isApprovedForAll(owner, msg.sender), "Msg.sender is not the owner or an approved operator");
+        require( msg.sender == owner || isApprovedForAll(owner, msg.sender), "Msg.sender is not the owner or an approved operator");
         _tokenApprovals[tokenId] = to;
         emit Approval(owner, to, tokenId);
     }
@@ -56,15 +62,17 @@ contract ERC721 {
     }
 
     // Transfers ownership of an NFT
-    function transferFrom(address from, address to, uint256 tokenId) public{
+    function transferFrom(address from, address to, uint256 tokenId) public {
         address owner = ownerOf(tokenId);
         require(
-            msg.sender == owner || getApproved(tokenId) == msg.sender || isApprovedForAll(owner, msg.sender),
+            msg.sender == owner ||
+            getApproved(tokenId) == msg.sender ||
+            isApprovedForAll(owner, msg.sender),
             "Msg.sender is not the owner or approved for transfer"
         );
         require(owner == from, "From address is not the owner");
         require(to != address(0), "Address is zero");
-        require(_owners[tokenId] != address(0), "TokenID does not exist" );
+        require(_owners[tokenId] != address(0), "TokenID does not exist");
         // because we transfer the nft, we need to clear all previous approvals
         approve(address(0), tokenId);
 
@@ -85,16 +93,18 @@ contract ERC721 {
     }
 
     function safeTransferFrom(address from, address to, uint256 tokenId) public {
-        safeTransferFrom(from,to,tokenId,"");
+        safeTransferFrom(from, to, tokenId, "");
     }
 
+    // Oversimplified
     //Oversimplified for test(should not be this in real environment)
     function _checkOnERC721Received() private pure returns(bool) {
         return true;
     }
 
     // EIP 165： Query if a contract implements another interface
-    function supportsInterface(bytes4 interfaceId) public pure virtual returns(bool){
-        return interfacedId == 0x80ac58cd;
+    function supportsInterface(bytes4 interfaceId) public pure virtual returns(bool) {
+        return interfaceId == 0x80ac58cd;
     }
+
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,27 +1,19 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
 
-async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+const {ethers} = require("hardhat");
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+async function main(){
 
-  await greeter.deployed();
+  //1st to do is to get a compiled contract
+  const SuperMarioWorld = await ethers.getContractFactory("SuperMarioWorld");
+  const superMarioWorld = await SuperMarioWorld.deploy("Alaska SeaLife Center", "ASC");
 
-  console.log("Greeter deployed to:", greeter.address);
+  await superMarioWorld.deployed();
+  console.log("Success! Contract was deployed to: ", superMarioWorld.address);
+
+  await superMarioWorld.mint("https://ipfs.io/ipfs/QmZRpUvBFmrHruBAKasN2zTuVrYgNiXQ7kTtZ36eHvnLVm");
+
+  console.log("NFT successfully minted");
 }
-
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main()
